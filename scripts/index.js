65e66ac4395f79a363c1b1eaf98639d3447c872f
sean bell <seanwizzle@hotmail.com>
2026-03-04 01:08:58 -0500

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,17 @@
-const editProfileBtn = document.querySelector(".edit__profile-btn");
-const editProfileModal = document.querySelector("#edit-profile-modal");
-const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const profileEditBtn = document.querySelector(".profile__edit-btn");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseBtn = profileEditModal.querySelector(".modal__close-btn");
 
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
-editProfileBtn.addEventListener("click", function () {
-  editProfileModal.classList.add("modal_is-opened");
+profileEditBtn.addEventListener("click", function () {
+  profileEditModal.classList.add("modal_is-opened");
 });
 
-editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+profileEditCloseBtn.addEventListener("click", function () {
+  profileEditModal.classList.remove("modal_is-opened");
 });
 
 newPostBtn.addEventListener("click", function () {
